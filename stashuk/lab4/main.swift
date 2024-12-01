73d8bb917d4e904cf07c4b1342443bbf3cd16243
December00 <154551839+December00@users.noreply.github.com>
2024-12-02 01:34:48 +0300

    Add files via upload

---
+++ stashuk/lab4/main.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+var num = Number<Int>(5)
+var result: Int = num.sum(6)
+print(result)
+result = num.min(1)
+print(result)
+
+result = num.mult(3)
+print(result)
+var dres: Double = num.div(4)
+print(dres)
+dres = num.power(2)
+print(dres)

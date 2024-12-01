b26b49946dbc0b649bfb16b609914fa7d8ef9136
December00 <154551839+December00@users.noreply.github.com>
2024-12-02 01:33:49 +0300

    Add files via upload

---
+++ stashuk/lab3/main.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+
+let fstEmpl = Employee("Коля", "Сисадмин", 4)
+let scndEmpl = Employee("Петя", "Секретарь", 11)
+let thrdEmpl = Employee("Святополк", "Полководец", 21)
+fstEmpl.printInfo()
+scndEmpl.printInfo()
+thrdEmpl.printInfo()
+
